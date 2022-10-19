9d2d7907cd380c88daf4a63ae4d1619cec44b67d
Nana <nanaolomaiyani@gmail.com>
2022-10-19 22:22:47 +0300

    commit tree e505ece45200c9c8ee975ea366fc7441b2de87e8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arry
+    arry.map{|name| badge_maker(name)}
+end
+
+def assign_rooms arry
+    arry.map.with_index{|name, ind|"Hello, #{name}! You'll be assigned to room #{ind + 1}!"}
+end
+
+def printer arry
+    arry.map{|name|puts badge_maker(name)}
+    arry.map.with_index{|name, ind|puts "Hello, #{name}! You'll be assigned to room #{ind + 1}!"}
+end
